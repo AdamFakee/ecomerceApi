90a0fdca4eff1f39fb63068987efb648c3d4f289
AdamFakee <buidinhtuan04@gmail.com>
2025-02-08 12:31:22 +0700

    bugfix: sửa lại router tránh chạy 2 lần router.use(authentication)

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -6,14 +6,14 @@ const asyncHandler = require('../../helpers/asyncHandler.helper');
 const { authentication } = require('../../auth/authUtils');
 const router = express.Router();
 
-router.post('/shop/signup', asyncHandler(accessController.signUp));
-router.post('/shop/login', asyncHandler(accessController.login));
+router.post('/signup', asyncHandler(accessController.signUp));
+router.post('/login', asyncHandler(accessController.login));
 
 // authentication
 router.use(authentication);
 
 //////////
-router.post('/shop/logout', asyncHandler(accessController.logout));
-router.post('/shop/handleRefreshToken', asyncHandler(accessController.handleRefreshToken));
+router.post('/logout', asyncHandler(accessController.logout));
+router.post('/handleRefreshToken', asyncHandler(accessController.handleRefreshToken));
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,6 @@ const router = express.Router();
 // // check permission
 // router.use(permissions('0000'));
 router.use('/v1/api/product', require('./product'));
-router.use('/v1/api/', require('./access'));
+router.use('/v1/api/shop', require('./access'));
 
 module.exports = router;
